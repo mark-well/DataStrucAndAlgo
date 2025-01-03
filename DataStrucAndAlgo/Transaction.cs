6d1aaa0f849a83e900a51032daa5663da9d1eabe
mark-well <lacemerto05@gmail.com>
2025-01-03 11:36:29 +0800

    database removed

---
+++ DataStrucAndAlgo/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucAndAlgo
+{
+    public class Transaction
+    {
+        public int id;
+        public string from;
+        public DateTime date;
+        public double amount;
+        public string reference;
+        public Transaction()
+        {
+
+        }
+    }
+}
